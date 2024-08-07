35e6e117271d93feaf4b47363f5dc9995fbed034
david <chuanthanh133@gmail.com>
2024-08-07 10:13:56 +0700

    chore: Update dependencies and add AuthController and AuthService to AuthModule

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+    signup() {
+        return {
+            message: 'signup',
+        };
+    }
+
+    signin() {
+        return 'signin';
+    }
+}
